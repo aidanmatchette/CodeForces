e246a0c1d2dadfed73311f6787dc69e43c2426b2
Aidan Matchette <frigasack@gmail.com>
2022-09-23 21:14:53 -0700

    Insomnia Cure in python

--- A/InsomniaCure.py
+++ A/InsomniaCure.py
@@ -6,7 +6,7 @@ class InsomniaCure:
     def solve(self, k, la, m, n, d):
         count = 0
         for i in range(1, d + 1):
-            if d % k == 0 or d % la == 0 or d % n == 0:
+            if i % k == 0 or i % la == 0 or i % n == 0:
                 count += 1
         return count
 
